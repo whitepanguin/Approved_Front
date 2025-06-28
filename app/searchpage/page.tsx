1cc711b5806dcbee81727ed2d2559c1f3b12635b
whitepanguin <dun04045@gmail.com>
2025-06-28 18:58:04 +0900

    b

--- app/searchpage/page.tsx
+++ app/searchpage/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+export const dynamic = "force-dynamic";
+
 import { useEffect, useState, useRef } from "react";
 import { useSearchParams } from "next/navigation";
 import MainLayout from "@/components/layout/main-layout";
