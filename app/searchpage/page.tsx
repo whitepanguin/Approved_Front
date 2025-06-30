c84c36a9b737bef0ab2ee7ce77c5914e151eef0b
손단하 <danhana@gmail.com>
2025-06-30 21:01:20 +0900

    url 수정

--- app/searchpage/page.tsx
+++ app/searchpage/page.tsx
@@ -155,7 +155,7 @@ export default function SearchPage() {
         setLoading(true);
 
         const res = await fetch(
-          `http://localhost:8001/searchllm?search=${encodeURIComponent(
+          `http://localhost:8000/searchllm?search=${encodeURIComponent(
             query
           )}&email=${encodeURIComponent(email)}`
         );
